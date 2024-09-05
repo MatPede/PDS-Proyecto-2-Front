638248a1f3b861cb83d7b4267d9deb964d729e4d
MaxmatuPede <maxmatugames@gmail.com>
2024-09-04 21:03:56 -0400

    arreglando

--- proyect2front/src/pages/HomePage.js
+++ proyect2front/src/pages/HomePage.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './HomePage.css';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import rozamientoPoleasImg from '../images/rozamientoypoleas.png';  
+import relojImg from '../images/reloj.png';
 
 const HomePage = () => {
   const navigate = useNavigate();
   const { isLogged } = useAuth();
 
-  console.log("isLogged homepage =", isLogged); // Imprime el valor de isLogged en la consola
 
   const handleNavigation = (path) => {
     navigate(path);
@@ -18,11 +19,13 @@ const HomePage = () => {
       <div className="card" onClick={() => handleNavigation('/dashboard')}>
         <div className="card-content">
           <h2>Rozamiento y poleas</h2>
+          <img src={rozamientoPoleasImg} alt="Rozamiento y Poleas" className="home-image" /> 
         </div>
       </div>
       <div className="card card-upcoming">
         <div className="card-content">
           <h2>Próximamente...</h2>
+          <img src={relojImg} alt="Cargando" className="reloj-image" />
         </div>
       </div>
     </div>
